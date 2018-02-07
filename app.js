c793106e60226bffb5ff092f52e09cff877adf15
Lucyloules <lucia.lagos@gmail.com>
2018-02-07 17:13:20 -0300

    encuentrame funcionando

--- app.js
+++ app.js
@@ -1,18 +1,43 @@
- function initMap() {
-        var uluru = {lat: -25.363, lng: 131.044};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 5,
-          center: uluru
-        });
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map
-        });
-      }
-/*El elemento script carga la API desde la URL especificada. 
-El parámetro callback ejecuta la función initMap cuando la API se carga por completo. 
-El atributo async permite al navegador seguir procesando el resto de la página mientras se carga la API. 
-El parámetro key contiene tu clave de API. 
-No necesitas tu propia clave de API al probar este instructivo en JSFiddle. 
-Consulta Paso 3: Obtener una clave de API para recibir instrucciones sobre cómo obtener tu propia clave 
-de API posteriormente.*/
+function initMap(){
+
+  var laboratoriaLima = {lat:-12.1191427, lng:-77.0349046};
+  
+  var map = new google.maps.Map(document.getElementById("map"),{
+    zoom: 3,
+    center: laboratoriaLima
+  });
+
+  var markadorLaboratoria = new google.maps.Marker({
+    position: laboratoriaLima,
+    map: map
+  });
+
+  function buscar(){
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
+    }
+  }
+  
+  document.getElementById("encuentrame").addEventListener("click", buscar);
+  var latitud, longitud;
+
+  var funcionExito = function(posicion){
+    latitud = posicion.coords.latitude;
+    longitud = posicion.coords.longitude;
+
+  var miUbicacion = new google.maps.Marker({
+    position: {lat:latitud, lng: longitud},
+    map:map
+  });
+
+  map.setZoom(18);
+  map.setCenter({lat:latitud, lng: longitud});
+  }
+
+var funcionError = function(error){//funcionError con un mensaje para el usuario, en caso de que nuestra geolocalización falle.
+    alert("tenemos un problema con encontrar tu ubicación");
+  }
+
+   //document.getElementById("encuentrame").addEventListener("click", buscar);
+}
+
